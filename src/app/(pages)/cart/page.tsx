dce642c2b57340a8366b4abef961d872afd5a830
Amman Abua <ammanabua@gmail.com>
2025-04-21 23:17:38 -0500

    modify cart page

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
+
+const EmptyCart = () => {
+
+
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+        <h1 className='text-center text-3xl text-[#EC770A]'>Your cart is empty!</h1>
+        <p className='text-center text-lg'>Looks like you haven&apos;t added anything to your cart yet.</p>
+        <Link href="/products" className='text-center text-lg hover:text-[#EC770A] hover:underline hover:underline-offset-2'>Start shopping now!</Link>
+    </div>
+  )
+
+}
+
 
 const page = () => {
   return (
-    <div>Your Cart is empty!</div>
+    <div><EmptyCart /></div>
   )
 }
 
