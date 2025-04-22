c57c272510d80f509bea45e6df1c58924456e476
Amman Abua <ammanabua@gmail.com>
2025-04-22 08:08:23 -0500

    update card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,19 +3,19 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className="border border-1 border-blue-600 p-4 rounded-xl w-[350px] space-y-4">
+    <div className="border border-1 border-blue-600 p-4 rounded-xl w-[350px] space-y-4 bg-white">
       <Image src="/indos.png" width={200} height={50} alt="name" className='m-auto' />
       <div className='flex items-center justify-between'>
         <h3 className='text-black font-semibold'>Fried Rice</h3>
-        <p className='text-2xl'>$34</p>
+        <p className='text-2xl text-black'>$34</p>
       </div>
-      <p>Delicious charcoal stove cooked fried rice</p>
+      <p className='text-[#808080]'>Delicious charcoal stove cooked fried rice</p>
       <div className=''>
-        <input type="number" className='bg-white rounded-lg text-black p-2 w-24' />
+        <input type="number" className='bg-white rounded-lg text-black p-2 w-24 border border-black' />
       </div>
       <div className='flex items-center justify-center gap-2'>
         <button className='bg-green-700 p-2 rounded-lg w-full cursor-pointer'>Add to Cart</button>
-        <button className='border border-1 p-2 rounded-lg w-full cursor-pointer'>Buy Now</button>
+        <button className='border border-1 p-2 rounded-lg w-full cursor-pointer bg-[#EC770A]'>Buy Now</button>
       </div>
     </div>
   )
